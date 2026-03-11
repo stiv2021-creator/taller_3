e45008680c1f44640093781e308c5522ad278bce
stiv2021-creator <stiv2021@gmail.com>
2026-03-11 18:06:19 -0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
-  base: '/taller_3/'
+  plugins: [react()],
+  base: '/taller_n3/'
 })
