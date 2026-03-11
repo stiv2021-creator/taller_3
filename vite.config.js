5ba22dd4ab410d2620285816e0a480aa818fe044
stiv2021-creator <stiv2021@gmail.com>
2026-03-11 18:39:47 -0500

    Fix base for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+const isVercel = process.env.VERCEL === '1';
+
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
-  base: isVercel ? '/' : '/taller_3/'
-})
+  plugins: [react()],
+  base: isVercel ? '/' : '/taller_3/' // Vercel "/" , GitHub Pages "/taller_3/"
+});
